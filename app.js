51ac12ea4a162d53af22b9586db477598959e5ee
Matthew Colvard <mustangf350@yahoo.com>
2018-04-04 10:58:35 -0500

    changed and modified and deleted

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-var book = require('./routes/book');
+// var book = require('./routes/book');
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/books', express.static(path.join(__dirname, 'dist')));
-app.use('/book', book);
+// app.use('/books', express.static(path.join(__dirname, 'dist')));
+// app.use('/book', book);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
